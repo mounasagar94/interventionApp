7daac39004fb85c8285a7bf5ef94714cc6b1724f
Mouna SAGAR <ms@odo-net.com>
2023-01-06 18:11:17 +0100

    add comments

--- interventionApp/serializers.py
+++ interventionApp/serializers.py
@@ -1,5 +1,6 @@
+## this is for making the data loaded from database readable
+## we will use it in views
 from rest_framework import serializers
-
 from interventionApp.models import Intervention
 
 
